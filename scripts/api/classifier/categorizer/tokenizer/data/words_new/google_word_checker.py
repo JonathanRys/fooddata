4364a490c5de480280551bd47c7ae9de214bf232
Jonathan Rys <jonathan.rk.rys@gmail.com>
2018-07-05 07:57:06 -0400

    Secure API key

--- scripts/api/classifier/categorizer/tokenizer/data/words_new/google_word_checker.py
+++ scripts/api/classifier/categorizer/tokenizer/data/words_new/google_word_checker.py
@@ -1,21 +1,14 @@
 import requests
 import json
-
+from google_api import key
 
 json_decoder = json.JSONDecoder()
 
 def get_url(query):
-    return "https://www.googleapis.com/customsearch/v1?key=AIzaSyDaYXKAxaPYsAFLz4ysVCWTMOht7lRo4SE&cx=009800307687027833122:z4j_j_yubki&q=" + query
+    return "https://www.googleapis.com/customsearch/v1?key=" + key + "&cx=009800307687027833122:z4j_j_yubki&q=" + query
 
 def get_data(url):
     return requests.get(url).text
-
-def read_data(file, delimiter="\n"):
-    """Read the data from a file and return a list"""
-    with open(file, 'rt', encoding="utf-8") as f:
-        data = f.read()
-
-    return data.split(delimiter)
 
 def get_correction(word):
     url = get_url(word)
@@ -24,11 +17,20 @@ def get_correction(word):
     if "spelling" in data and "correctedQuery" in data["spelling"]:
         print(word, "=", data["spelling"]["correctedQuery"])
 
+
+def read_data(file, delimiter="\n"):
+    """Read the data from a file and return a list"""
+    with open(file, 'rt', encoding="utf-8") as f:
+        data = f.read()
+
+    return data.split(delimiter)
+
 def google_word_checker():
     data = read_data("unknown.txt")
     for x in data:
         get_correction(x)
 
+
 if __name__ == '__main__':
-    #get_correction("acidstarter")
-    google_word_checker()
+    get_correction("acidstarter")
+    #google_word_checker()
